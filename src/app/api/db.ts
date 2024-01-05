f4f768f72ca5015e389286da8c859d9c2182524a
h0ngwon <w___on@naver.com>
2024-01-05 12:35:01 +0900

    Chore : 프로젝트 세팅

---
+++ src/app/api/db.ts
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js';
+import { Database } from '../types/supabase';
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY as string;
+const supabase = createClient<Database>(supabaseUrl, supabaseKey);
+
+export { supabase };
