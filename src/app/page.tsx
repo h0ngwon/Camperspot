0d6b177594a50be5ab7504bfee56bc23677b1dc1
coldbrew <minsuk815@naver.com>
2024-01-08 17:08:16 +0900

    feat: 헤더에 개발 페이지 라우팅 (#1)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,3 @@
-import Reservation from './components/Reservation';
-import './styles/reset.css';
-
 export default function Home() {
-  return (
-    <main>
-      <Reservation />
-    </main>
-  );
+  return <main></main>;
 }
