4f549d3b06dcd1183ed8eab4663cb9681aba0327
정유진 <jane320@naver.com>
2024-01-13 02:04:48 +0900

    fix: 예약 모달창  패러렐 라우트 오류 수정 (#6)

--- src/app/camp/detail/[id]/reservation/page.tsx
+++ src/app/camp/detail/[id]/reservation/page.tsx
@@ -1,10 +1,10 @@
-// import ReservationModal from '../_components/ReservationModal';
-
 import ReservationModal from '../_components/ReservationModal';
 
 const page = () => {
   return (
     <>
+      {/* 나중에 캠프 디테일 페이지 컴포넌트 추가할 예정 */}
+      <button>예약하기</button>
       <ReservationModal />
     </>
   );
