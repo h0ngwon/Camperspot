6b4ee8fddd8af36b1d18252df5e0e4a68e1cd715
mirae <mirae9873@gmail.com>
2024-01-13 21:57:20 +0900

    feat:company_user id 가져오기 (#2)

--- src/app/company/[id]/manage_camp/add/page.tsx
+++ src/app/company/[id]/manage_camp/add/page.tsx
@@ -2,12 +2,20 @@
 import { supabase } from '@/app/api/db';
 import useInput from '@/hooks/useInput';
 import type { Tables, TablesInsert } from '@/types/supabase';
-import React, { FormEvent, useEffect, useState } from 'react';
+import React, {
+  ChangeEvent,
+  FormEvent,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 import styles from './_components/campForm.module.css';
 import Head from 'next/head';
 import AddressModal from '@/app/company/[id]/manage_camp/add/_components/AddressModal';
 import { Address } from 'react-daum-postcode';
 import { uuid } from 'uuidv4';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 const addCampPage = () => {
   const [name, handleName] = useInput();
@@ -24,7 +32,12 @@ const addCampPage = () => {
   const [layout, handleLayout] = useInput();
   const [isAddressModal, setAddressModal] = useState(false);
   const [facility, setFacility] = useState<Tables<'facility'>[]>();
-  const [checkedFacility, setCheckedFacility] = useState<string[]>([]);
+  const [checkedFacility, setCheckedFacility] = useState<number[]>([]);
+  const [campPicture, setCampPicture] = useState<Tables<'camp_pic'>[]>();
+
+  const campId = uuid();
+
+  const { data: session } = useSession();
 
   async function fetchFacilityData() {
     // facility에서 option 가져오는거
@@ -33,15 +46,33 @@ const addCampPage = () => {
       setFacility(facilityData);
     }
   }
+  // const imgRef = useRef<HTMLInputElement>(null);
 
+  const uploadCampPic = async (e: ChangeEvent<HTMLInputElement>) => {
+    // const file = imgRef.current?.files;
+    if (e.target.files) {
+      const file = e.target.files[0];
+      return file;
+    }
+  };
+
+  async function fetchCampPicData() {
+    const { data: campPictureData } = await supabase
+      .from('camp_pic')
+      .select('*');
+    if (campPictureData) {
+      setCampPicture(campPictureData);
+    }
+  }
   useEffect(() => {
     fetchFacilityData();
+    fetchCampPicData();
   }, []);
 
-  const onHandleCheckFacility = (value: string) => {
-    if (checkedFacility.find((item) => item === value)) {
+  const onHandleCheckFacility = (value: number) => {
+    if (checkedFacility.find((item) => Number(item) === value)) {
       const filterdFacility = checkedFacility.filter((item) => {
-        return item !== value;
+        return Number(item) !== value;
       });
       return setCheckedFacility(filterdFacility);
     }
@@ -52,20 +83,27 @@ const addCampPage = () => {
   const handleForm = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    // TODO : company_id는 나중에 가져온느 로직을 작성해야 한다.
-    const companyId = '1fc2b9ca-341a-490c-b995-370eb36e1d5d';
+    //
+    const companyEmail = session?.user?.email as string;
+    const { data: getCompanyId } = await supabase
+      .from('company_user')
+      .select('id')
+      .eq('email', companyEmail);
+    if (!getCompanyId) {
+      return;
+    }
+    const companyId = getCompanyId[0].id;
+
     const regionSplit = address.split(' ');
     const regionDoGun = regionSplit[0] + ' ' + regionSplit[1];
-    const id = uuid();
-    console.log(id);
 
     const { data: campData, error } = await supabase
       .from('camp')
       .insert({
-        id,
+        id: campId,
         name,
         content,
-        company_id: companyId,
+        company_id: companyId as string,
         address,
         region: regionDoGun,
         phone,
@@ -79,10 +117,18 @@ const addCampPage = () => {
       .from('camp_facility')
       .insert(
         checkedFacility.map((item) => {
-          return { camp_id: id, facility_id: item };
+          return { camp_id: campId, facility_id: item };
         }),
       )
       .select();
+
+    // const avatarFile = event.target.files[0];
+    // const { data: uploadData } = await supabase.storage
+    //   .from('camp_pic')
+    //   .upload('photo_url', avatarFile, {
+    //     cacheControl: '3600',
+    //     upsert: false,
+    //   });
 
     if (campData && camp_facility) {
       alert('등록되었습니다');
@@ -118,88 +164,110 @@ const addCampPage = () => {
       <Head>
         <script src='//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js'></script>
       </Head>
+
       <form onSubmit={handleForm} className={styles.formLayout}>
-        <input
-          defaultValue={name}
-          onChange={handleName}
-          className={styles.gridItem}
-          placeholder='캠핑장 이름을 입력해주세요'
-        />
+        <h1>캠핑장 등록</h1>
+        <div>
+          <h3>캠핑장 명</h3>
+          <input
+            defaultValue={name}
+            onChange={handleName}
+            placeholder='이름을 입력해주세요'
+          />
+        </div>
 
         <div>
-          <button
-            onClick={() => {
-              setAddressModal(true);
-            }}
-            type='button'
-          >
-            캠핑장 주소찾기
-          </button>
+          <h3>캠핑장 주소</h3>
+          <div>
+            <div>
+              <button
+                onClick={() => {
+                  setAddressModal(true);
+                }}
+                type='button'
+              >
+                주소 검색하기
+              </button>
+            </div>
+            <input
+              defaultValue={address}
+              placeholder='주소검색하기를 클릭해주세요'
+            />
+          </div>
+        </div>
+        <div>
+          <h3>캠핑장 소개</h3>
+          <textarea
+            defaultValue={content}
+            onChange={handleContent}
+            className={styles.gridItemTextArea}
+            placeholder='캠핑장을 소개해주세요'
+          ></textarea>
+        </div>
+        <div>
+          <h3>시설 등록</h3>
+          <div className={styles.facilityWrap}>
+            {facility?.map((item, index) => {
+              return (
+                <div key={index} className={styles.facility}>
+                  <input
+                    type='checkbox'
+                    id={item.id.toString()}
+                    onChange={() => {
+                      onHandleCheckFacility(item.id);
+                    }}
+                  />
+                  <label htmlFor={item.id.toString()}>{item.option}</label>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div>
+          <h3>체크인 시간</h3>
           <input
-            defaultValue={address}
-            placeholder='캠핑장 주소찾기를 클릭해주세요'
+            defaultValue={check_in}
+            onChange={handleCheck_in}
+            placeholder='체크인 시간을 입력해주세요'
           />
-          {/* <input
-            defaultValue={addressDetail}
-            onChange={handleAddressDetail}
-            placeholder='상세주소를 입력해주세요'
-            
-          /> */}
+          <h3>체크아웃 시간</h3>
+          <input
+            defaultValue={check_out}
+            onChange={handleCheck_out}
+            placeholder='체크아웃 시간을 입력해주세요'
+          />
         </div>
-        <textarea
-          defaultValue={content}
-          onChange={handleContent}
-          className={styles.gridItemTextArea}
-          placeholder='캠핑장을 소개해주세요'
-        ></textarea>
-        <div className={styles.facilityWrap}>
-          {facility?.map((item, index) => {
-            return (
-              <div key={index} className={styles.facility}>
-                <input
-                  type='checkbox'
-                  id={item.id}
-                  onChange={() => {
-                    onHandleCheckFacility(item.id);
-                  }}
-                />
-                <label htmlFor={item.id}>{item.option}</label>
-              </div>
-            );
-          })}
+        <div>
+          <h3>문의전화</h3>
+          <input
+            defaultValue={phone}
+            onChange={handlePhone}
+            type='tel'
+            placeholder='캠핑장 전화번호를 입력해주세요. 예) 02-000-0000 / 063-000-0000'
+            pattern='[0-9]{2,3}-[0-9]{3,4}-[0-9]{4}'
+            maxLength={13}
+          />
         </div>
-        <input
-          defaultValue={phone}
-          onChange={handlePhone}
-          type='tel'
-          className={styles.gridItem}
-          placeholder='캠핑장 전화번호를 입력해주세요. 예) 02-000-0000 / 063-000-0000'
-          pattern='[0-9]{2,3}-[0-9]{3,4}-[0-9]{4}'
-          maxLength={13}
-        />
+        <div>
+          <h3>캠핑장 배치 사진 등록</h3>
+          <label htmlFor='uploadCampPic'>이미지 업로드</label>
+          <input
+            id='uploadCampPic'
+            type='file'
+            onChange={uploadCampPic}
+            // ref={imgRef}
+          />
+          {/* {campPicture?.filter((item) => {
+            if (item.camp_id === campId) {
+              return <img src={item.photo_url} />;
+            }
+          })} */}
+        </div>
 
-        <input
-          defaultValue={check_in}
-          onChange={handleCheck_in}
-          className={styles.gridItem}
-          placeholder='체크인 가능시간을 입력해주세요'
-        />
-        <input
-          defaultValue={check_out}
-          onChange={handleCheck_out}
-          className={styles.gridItem}
-          placeholder='체크아웃 시간을 입력해주세요'
-        />
-        {/* <input
-          defaultValue={layout}
-          onChange={handleLayout}
-          className={styles.gridItem}
-          placeholder='캠핑장 구조를 입력해주세요'
-        /> */}
-
-        <button type='submit' className={styles.gridItem}>
-          등록
-        </button>
+        <div>
+          <button type='button'>임시저장</button>
+          <button type='submit'>등록하기</button>
+        </div>
       </form>
       {isAddressModal && (
         <div className={styles.modalUP} onClick={handleClickOutsideModal}>
