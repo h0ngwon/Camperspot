cb5493b82db47e340a9e058e163cf6793bf832ae
정유진 <jane320@naver.com>
2024-01-18 18:10:13 +0900

    fix: 일부 코드  수정 (#13)

--- src/app/camp/detail/[id]/reservation/page.tsx
+++ src/app/camp/detail/[id]/reservation/page.tsx
@@ -1,10 +1,10 @@
 import ReservationModal from '../_components/ReservationModal';
+import DetailPage from '../page';
 
 const page = () => {
   return (
     <>
-      {/* 나중에 캠프 디테일 페이지 컴포넌트 추가할 예정 */}
-      <button>예약하기</button>
+      <DetailPage />
       <ReservationModal />
     </>
   );
