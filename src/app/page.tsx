55ccd2488f9d2170348617a599054303a1b21feb
coldbrew <minsuk815@naver.com>
2024-01-18 21:56:53 +0900

    fix: main 세션제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,5 @@
-'use client';
-import { useSession } from 'next-auth/react';
 import Main from './_components/Main';
 
 export default function Home() {
-  const { data: session } = useSession();
-
-  if (session) {
-    console.log(session);
-  }
   return <Main />;
 }
