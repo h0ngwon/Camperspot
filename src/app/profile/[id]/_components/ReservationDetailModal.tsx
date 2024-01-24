abaf67d7ca5c5d7d1149e2ce4cc1fdc44a147271
정유진 <jane320@naver.com>
2024-01-24 21:25:36 +0900

    style: 예약 상세 모달 스타일 완료 및 클래스명 변경 (#41)

--- src/app/profile/[id]/_components/ReservationDetailModal.tsx
+++ src/app/profile/[id]/_components/ReservationDetailModal.tsx
@@ -28,18 +28,18 @@ const ReservationDetailModal = ({
     <>
       <p className={styles.info}>예약 상세 보기</p>
       <div className={styles.modal}>
-        <button className={styles.closeBtn} onClick={() => onClose()}>
+        <button className={styles['close-btn']} onClick={() => onClose()}>
           <ModalCloseSvg />
         </button>
-        <div className={styles.div}>
-          <div className={styles.campInfo}>
+        <div>
+          <div className={styles['camp-info']}>
             <h3 className={styles.h3}>예약 정보</h3>
-            <p className={styles.p}>
-              <span className={styles.span}>객실</span>{' '}
-              <span className={styles.campName}>{campName}</span>
+            <p>
+              <span className={styles.span}>캠핑존 </span>{' '}
+              <span className={styles['camp-name']}>{campName}</span>
               {campAreaName}
             </p>
-            <p className={styles.p}>
+            <p>
               <span className={styles.span}>인원</span> {people}명
             </p>
 
@@ -72,32 +72,31 @@ const ReservationDetailModal = ({
               </div>
             </div>
           </div>
-          <div className={styles.payInfo}>
+          <div className={styles['person-info']}>
             <h3 className={styles.h3}>예약자 정보</h3>
-            <p>예약자명 {client_name}</p>
-            <p>연락처 {client_phone}</p>
+            <div className={styles.person}>
+              예약자명 <p>{client_name}</p>
+            </div>
+            <div className={styles.person}>
+              연락처 <p>{client_phone}</p>
+            </div>
           </div>
         </div>
-        <div className={styles.payInfo}>
+        <div className={styles['pay-info']}>
           <h3 className={styles.h3}>결제 정보</h3>
-          <div className={styles['pay-info']}>
-            <div className={styles.pay}>
-              결제일시 <p>{new Date(created_at).toLocaleString()}</p>
-            </div>
-            <div className={styles.pay}>
-              상품 가격 <p>{fee.toLocaleString()}원</p>
-            </div>
-            <div className={styles.pay}>
-              결제 수단 <p>{payment_method}</p>
-            </div>
+
+          <div className={styles.pay}>
+            결제일시 <p>{new Date(created_at).toLocaleString()}</p>
+          </div>
+          <div className={styles.pay}>
+            상품 가격 <p>{fee.toLocaleString()}원</p>
+          </div>
+          <div className={styles.pay}>
+            결제 수단 <p>{payment_method}</p>
           </div>
         </div>
         <div className={styles.divider}></div>
-
-        <div className={styles.prices}>
-          {/* <p className={styles.total}>
-            예약 금액 <span>{fee.toLocaleString()}원</span>
-          </p> */}
+        <div>
           <p className={styles.total}>
             총 결제 금액{' '}
             <span className={styles.price}>{fee.toLocaleString()}원 </span>
