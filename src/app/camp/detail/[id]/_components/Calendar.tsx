871bb536db68252b4e7b8119df60663b35816bb3
정유진 <jane320@naver.com>
2024-01-28 09:13:32 +0900

    fix: 예약 날짜 여부 확인 유효성 검사 오류 수정 (#62)

--- src/app/camp/detail/[id]/_components/Calendar.tsx
+++ src/app/camp/detail/[id]/_components/Calendar.tsx
@@ -22,10 +22,9 @@ export const Calendar = ({ control }: { control: Control<UserInfo> }) => {
   const params = useSearchParams();
   const id = params.get('id');
 
-  const isValidDate = (startDate: Date) => {
-    //체크아웃 날짜가 오늘 날짜(날짜 기본값 체크인)보다 크거나 같은 경우 &&
-    //체크인 날짜가 오늘 날짜(날짜 기본값 체크인)보다 크거나 같은 경우
-    if (!startDate) return false;
+  const isValidDate = (dates: [Date, Date]) => {
+    const [start, end] = dates;
+    if (!start || !end) return false;
     const data =
       excludeDates?.filter((date) => {
         const check_out_date = new Date(date.check_out_date).setHours(
@@ -35,14 +34,15 @@ export const Calendar = ({ control }: { control: Control<UserInfo> }) => {
           0,
         );
         const check_in_date = new Date(date.check_in_date).setHours(0, 0, 0, 0);
-        const start_date = startDate.setHours(0, 0, 0, 0);
+        const start_date = start.setHours(0, 0, 0, 0);
+        const end_date = end.setHours(0, 0, 0, 0);
         return (
-          new Date(check_out_date).getTime() >=
-            new Date(start_date).getTime() &&
-          new Date(start_date).getTime() >= new Date(check_in_date).getTime()
+          // 캘린더로 선택한 날짜가 예약된 날짜 범위에 해당되는지 확인
+          new Date(check_in_date).getTime() <= new Date(start_date).getTime() &&
+          new Date(check_out_date).getTime() >= new Date(end_date).getTime()
         );
       }) || [];
-
+    console.log('data', data);
     // 겹치는 날짜가 없을 때만 유효하도록 설정
     return data!.length > 0 ? false : true;
   };
@@ -57,7 +57,7 @@ export const Calendar = ({ control }: { control: Control<UserInfo> }) => {
         name='dates'
         control={control}
         rules={{
-          validate: (value) => isValidDate(value[0]),
+          validate: (value) => isValidDate(value),
         }}
         render={({ field: { onChange, value } }) => (
           <>
@@ -70,17 +70,14 @@ export const Calendar = ({ control }: { control: Control<UserInfo> }) => {
               selectsRange
               excludeDateIntervals={excludeDates?.map((exclude) => ({
                 start: subDays(new Date(exclude.check_in_date), 1),
-                end: new Date(exclude.check_out_date),
+                end: subDays(new Date(exclude.check_out_date), 1),
               }))}
               locale='ko'
               showIcon
               icon={svg}
             />
-            {/* invalid가 처음부터 끝까지 false로 나옴 원래 처음에는 true로 나와야함 */}
             {/* 유효하지 않을 때(이미 예약된 날짜와 겹치는 경우) 안내 문구 */}
-            {value[0] && !isValidDate(value[0]) && (
-              <p>이미 예약된 날짜입니다.</p>
-            )}
+            {!isValidDate(value) && <p>이미 예약된 날짜입니다.</p>}
             {(!value[0] || !value[1]) && <p>날짜를 선택해주세요.</p>}
           </>
         )}
