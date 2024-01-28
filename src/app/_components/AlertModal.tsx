3ff2fac649aa1d1788e31ec966175663a0ad1d10
정유진 <jane320@naver.com>
2024-01-28 21:48:30 +0900

    style: 예약 완료 모달창 스타일 수정 (#62)

--- src/app/_components/AlertModal.tsx
+++ src/app/_components/AlertModal.tsx
@@ -1,46 +1,44 @@
-'use client';
-import styles from '../_styles/ConfirmModal.module.css';
-import { useRouter } from 'next/navigation';
+// 'use client';
+import ModalCloseSvg from '@/components/ModalCloseSvg';
+import styles from '../_styles/AlertModal.module.css';
+import Link from 'next/link';
+import { useSession } from 'next-auth/react';
+import CampingImg from '@/asset/camping_illust.jpg';
+import Image from 'next/image';
 
 type Props = {
   title: string;
+  onClose: () => void;
 };
 
-const AlertModal = ({ title }: Props) => {
-  const router = useRouter();
-  setTimeout(() => {
-    router.push('/');
-  }, 3000);
-  if (!open) return null;
+const AlertModal = ({ title, onClose }: Props) => {
+  const { data: session } = useSession();
+  console.log('sesssion', session);
 
   return (
     <div className={styles.div}>
-      {/* 나중에 로고 변경될 수도 있으니 그냥 태그로 넣음 */}
-      <svg
-        width='100px'
-        height='100px'
-        viewBox='0 0 20 20'
-        xmlns='http://www.w3.org/2000/svg'
-        fill='none'
-      >
-        <g id='SVGRepo_bgCarrier' strokeWidth='0'></g>
-        <g
-          id='SVGRepo_tracerCarrier'
-          strokeLinecap='round'
-          strokeLinejoin='round'
-        ></g>
-        <g id='SVGRepo_iconCarrier'>
-          {' '}
-          <path
-            stroke='#77B255'
-            strokeLinecap='round'
-            strokeLinejoin='round'
-            strokeWidth='2'
-            d='M17 5L8 15l-5-4'
-          ></path>{' '}
-        </g>
-      </svg>
+      {/* 나중에 이미지로 변경될 예정 */}
+
+      <h3 className={styles.h3}>예약 및 결제</h3>
+      <div className={styles.close} onClick={onClose}>
+        <ModalCloseSvg />
+      </div>
+
+      <Image
+        className={styles.img}
+        src={CampingImg}
+        alt='image'
+        width={150}
+        height={150}
+      />
+
       <h2 className={styles.h2}>{title}</h2>
+      <Link
+        className={styles.link}
+        href={`/profile/${session?.user.id}/reservation`}
+      >
+        예약현황 바로가기
+      </Link>
     </div>
   );
 };
