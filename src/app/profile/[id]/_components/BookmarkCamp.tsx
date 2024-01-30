39acdba1f5377073132207af226ee00ce9679eb5
h0ngwon <w___on@naver.com>
2024-01-30 16:19:28 +0900

    feat: 캠핑장 찜 목록 기능 (#24)

--- src/app/profile/[id]/_components/BookmarkCamp.tsx
+++ src/app/profile/[id]/_components/BookmarkCamp.tsx
@@ -3,23 +3,30 @@ import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'next/navigation';
 import { getUserLikeCamp } from '../_lib/getUserLikeCamp';
 import styles from '../_styles/BookmarkCamp.module.css';
-import { LikeCampType } from '@/types/profile';
+import CampCard from '@/app/_components/CampCard';
+import Card from '@/components/Card';
+import BookmarkCampContent from './BookmarkCampContent';
+import Camp from '@/app/camp/page';
 
 const BookmarkCamp = () => {
   const params = useParams();
+  const userId = params.id as string;
   const { data, isLoading } = useQuery({
-    queryKey: ['mypage', 'bookmark', params.id as string],
-    queryFn: () => getUserLikeCamp(params.id as string)
+    queryKey: ['mypage', 'bookmark', userId],
+    queryFn: getUserLikeCamp,
+    refetchOnWindowFocus: true,
   });
 
   if (isLoading) {
     <p>로딩중...</p>;
   }
-  console.log('data==============', data);
+  console.log(data);
   return (
     <div className={styles.container}>
       <div className={styles['bookmark-header']}>찜 목록</div>
-      <div className={styles['bookmark-wrapper']}></div>
+      <div className={styles['bookmark-wrapper']}>
+        {data?.map((content) => <BookmarkCampContent key={content.camp.id} camp={content.camp}/>)}
+      </div>
     </div>
   );
 };
