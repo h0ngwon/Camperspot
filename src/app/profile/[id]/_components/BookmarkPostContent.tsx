b782670b56d2dbf242d451fd210283bf0b027a4f
h0ngwon <w___on@naver.com>
2024-01-31 17:25:05 +0900

    feat: 커뮤니티 찜 목록 및 이미지 최적화 적용 (#24)

--- src/app/profile/[id]/_components/BookmarkPostContent.tsx
+++ src/app/profile/[id]/_components/BookmarkPostContent.tsx
@@ -2,6 +2,7 @@
 import Card from '@/components/Card';
 import { LikePostType } from '@/types/profile';
 import styles from '../_styles/BookmarkPostContent.module.css';
+import Image from 'next/image';
 
 type Props = {
   post: LikePostType[number]['post'];
@@ -10,7 +11,24 @@ type Props = {
 const BookmarkPostContent = ({ post }: Props) => {
   return (
     <Card>
-      <div className={styles.container}>{post?.id}</div>
+      <div className={styles.container}>
+        <div className={styles['img-wrapper']}>
+          <Image
+            src={post.post_pic[0].photo_url}
+            width={174}
+            height={174}
+            alt='커뮤니티 사진'
+            priority={true}
+            quality={100}
+          />
+        </div>
+        <div className={styles['content']}>{post.content}</div>
+        <div className={styles['tags']}>
+          {post.post_hashtag.map((item, idx) => {
+            return <span key={`post_id_${idx}`}>#{item.tag}{' '}</span>;
+          })}
+        </div>
+      </div>
     </Card>
   );
 };
