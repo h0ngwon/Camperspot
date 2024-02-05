a2504035db56fa9a78e20914dfdf7c648ab4c593
mirae <mirae9873@gmail.com>
2024-02-05 15:08:12 +0900

    style: added_camp 페이지 로딩중 일때 외부에 로딩중 띄워놓기- 튜터님 피드백 반영, 추후 스켈레톤이나 외부 ui 추가 예정(#19)

--- src/app/company/[id]/manage_camp/added_camp/page.tsx
+++ src/app/company/[id]/manage_camp/added_camp/page.tsx
@@ -65,71 +65,75 @@ const ManageAddedCamp = () => {
 
   return (
     <>
-      <h1 className={styles.h1}>캠핑장 운영 및 관리</h1>
-      {data?.length === 0 ? (
-        <div className={styles.emptyCampContainer}>
-          <p className={styles.p}>등록된 캠핑장이 없습니다.</p>
-          <EmptyCampIllustSvg />
-          <button
-            onClick={() => {
-              router.push(`/company/${companyUserId}/manage_camp/add_camp`);
-            }}
-            className={styles.button}
-          >
-            캠핑장 등록하기
-            <RightArrowSvg />
-          </button>
-        </div>
+      {isLoading ? (
+        <div>로딩중</div>
       ) : (
-        <div className={styles.campWrap}>
-          <div className={styles.campHeader}>
-            <div className={styles.campHeaderLeft}>캠핑장</div>
-            <div className={styles.campHeaderRight}>편집 기능</div>
-          </div>
-          <div className={styles.separateLine}></div>
-          {data?.map((item) => {
-            return (
-              <div key={item.id}>
-                <div className={styles.campBox}>
-                  <div className={styles.campImg}>
-                    <Image
-                      src={item.camp_pic[0]?.photo_url}
-                      alt='캠핑장 이미지'
-                      width='0'
-                      height='0'
-                      sizes='100vw'
-                      style={{ width: '100%', height: '100vh' }}
-                      priority
-                    />
-                  </div>
-
-                  <div className={styles.campNameWrap}>
-                    <h1 className={styles.campName}>{item.name}</h1>
-                    <button
-                      onClick={() => handleGoToCampDetail(item.id)}
-                      className={styles.preview}
-                    >
-                      <span>미리보기</span>
-                      <PreviewSvg />
-                    </button>
-                  </div>
-
-                  <div className={styles.editWrap}>
-                    <button onClick={() => goToUpdateCampPage(item.id)}>
-                      캠핑장 정보 수정
-                    </button>
-                    <button onClick={() => goToUpdateCampAreaPage(item.id)}>
-                      캠핑존 정보 수정
-                    </button>
-                  </div>
-                </div>
-                <div className={styles.separateLineTwo}></div>
+        <div>
+          <h1 className={styles.h1}>캠핑장 운영 및 관리</h1>
+          {data?.length === 0 ? (
+            <div className={styles.emptyCampContainer}>
+              <p className={styles.p}>등록된 캠핑장이 없습니다.</p>
+              <EmptyCampIllustSvg />
+              <button
+                onClick={() => {
+                  router.push(`/company/${companyUserId}/manage_camp/add_camp`);
+                }}
+                className={styles.button}
+              >
+                캠핑장 등록하기
+                <RightArrowSvg />
+              </button>
+            </div>
+          ) : (
+            <div className={styles.campWrap}>
+              <div className={styles.campHeader}>
+                <div className={styles.campHeaderLeft}>캠핑장</div>
+                <div className={styles.campHeaderRight}>편집 기능</div>
               </div>
-            );
-          })}
-          <button className={styles.moreAddCamp} onClick={moreAddCamp}>
-            캠핑장 추가 등록
-          </button>
+              <div className={styles.separateLine}></div>
+              {data?.map((item) => {
+                return (
+                  <div key={item.id}>
+                    <div className={styles.campBox}>
+                      <div className={styles.campImg}>
+                        <Image
+                          src={item.camp_pic[0]?.photo_url}
+                          alt='캠핑장 이미지'
+                          width='0'
+                          height='0'
+                          sizes='100vw'
+                          style={{ width: '100%', height: '100vh' }}
+                          priority
+                        />
+                      </div>
+                      <div className={styles.campNameWrap}>
+                        <h1 className={styles.campName}>{item.name}</h1>
+                        <button
+                          onClick={() => handleGoToCampDetail(item.id)}
+                          className={styles.preview}
+                        >
+                          <span>미리보기</span>
+                          <PreviewSvg />
+                        </button>
+                      </div>
+                      <div className={styles.editWrap}>
+                        <button onClick={() => goToUpdateCampPage(item.id)}>
+                          캠핑장 정보 수정
+                        </button>
+                        <button onClick={() => goToUpdateCampAreaPage(item.id)}>
+                          캠핑존 정보 수정
+                        </button>
+                      </div>
+                    </div>
+                    <div className={styles.separateLineTwo}></div>
+                  </div>
+                );
+              })}
+              <button className={styles.moreAddCamp} onClick={moreAddCamp}>
+                캠핑장 추가 등록
+              </button>
+            </div>
+          )}
         </div>
       )}
     </>
