9748f17836c1dd6f2d18d17236239d8c7627617d
coldbrew <minsuk815@naver.com>
2024-02-05 14:18:00 +0900

    style:404 css 수정(#127)

--- src/app/_components/Carousel.tsx
+++ src/app/_components/Carousel.tsx
@@ -17,7 +17,7 @@ const Carousel = () => {
   const settings = {
     dots: true,
     infinite: true,
-    slidesToShow: 3,
+    slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
--- src/app/_components/ProfileDivider.tsx
+++ src/app/_components/ProfileDivider.tsx
@@ -6,6 +6,7 @@ import styles from '../_styles/Header.module.css';
 import { Session } from 'next-auth';
 import { useQuery } from '@tanstack/react-query';
 import { getUserData } from '../profile/[id]/_lib/profile';
+import { signOut } from 'next-auth/react';
 
 type Props = {
   session: Session;
@@ -35,6 +36,32 @@ const ProfileDivider = ({ session }: Props) => {
           <p>마이</p>
         </Link>
       )}
+      {/* <div style={{width: 118, height: 182, flexDirection: 'column', justifyContent: 'center', alignItems: 'center', display: 'inline-flex'}}>
+  <div style={{flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-end', display: 'flex'}}>
+    <div style={{flexDirection: 'column', justifyContent: 'center', alignItems: 'center', display: 'flex'}}>
+      <div style={{justifyContent: 'flex-start', alignItems: 'flex-start', gap: 12, display: 'inline-flex'}}>
+        <div style={{width: 36, height: 36, position: 'relative'}}>
+          <div style={{width: 30, height: 29, left: 3, top: 3, position: 'absolute', background: 'black'}}></div>
+        </div>
+      </div>
+      <div style={{color: 'black', fontSize: 16, fontFamily: 'Pretendard', fontWeight: '400', wordWrap: 'break-word'}}>사장님</div>
+    </div>
+    <div style={{width: 118, height: 127, paddingTop: 8, paddingBottom: 8, background: 'white', boxShadow: '0px 2px 3px rgba(0, 0, 0, 0.25)', borderRadius: 8, flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', display: 'flex'}}>
+      <div style={{width: 118, paddingLeft: 24, paddingRight: 24, paddingTop: 10, paddingBottom: 10, background: 'white', justifyContent: 'center', alignItems: 'center', gap: 10, display: 'inline-flex'}}>
+        <div style={{color: 'black', fontSize: 14, fontFamily: 'Pretendard', fontWeight: '400', wordWrap: 'break-word'}}>계정관리</div>
+      </div>
+      <div style={{width: 118, paddingLeft: 24, paddingRight: 24, paddingTop: 10, paddingBottom: 10, background: 'white', justifyContent: 'center', alignItems: 'center', gap: 10, display: 'inline-flex'}}>
+        <div style={{color: 'black', fontSize: 14, fontFamily: 'Pretendard', fontWeight: '400', wordWrap: 'break-word'}}>캠핑장 이용</div>
+      </div>
+      <div style={{width: 118, paddingLeft: 24, paddingRight: 24, paddingTop: 10, paddingBottom: 10, background: 'white', justifyContent: 'center', alignItems: 'center', gap: 10, display: 'inline-flex'}}>
+        <div style={{color: 'black', fontSize: 14, fontFamily: 'Pretendard', fontWeight: '400', wordWrap: 'break-word'}}>로그아웃</div>
+      </div>
+    </div>
+  </div>
+</div>z
+      <button onClick={() => signOut()} className={styles.logOutBtn}>
+        a
+      </button> */}
     </>
   );
 };
